bf4847c1a0cbc04cf4dcfc5f0b06610a77dbce12
eedo_y <doup2001@naver.com>
2023-11-11 00:14:46 +0900

    DONE

---
+++ GAME_Client.c
@@ -0,0 +1,205 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 12345
+#define MAX_PLAYERS 10
+
+struct Player {
+    char name[40];
+    int attempts;
+};
+
+struct Player players[MAX_PLAYERS];  // 플레이어 정보 배열
+int numPlayers = 0;  // 현재까지의 플레이어 수
+
+
+void displayMenu_cl();
+void startGame_cl(int server_socket);
+void SearchMyRecord_cl(int server_socket);
+void calculateAndFindMinAttempts_cl(int server_socket);
+
+
+int main() {
+    int server_socket;
+    struct sockaddr_in server_addr;
+
+    // 클라이언트 소켓 생성
+    if ((server_socket = socket(PF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("Client socket creation error");
+        exit(1);
+    }
+
+    // 서버 주소 설정
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    // 서버에 연결
+    if (connect(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+        perror("Client connect error");
+        close(server_socket);
+        exit(1);
+    }
+
+    int choice;
+
+while (1) {
+    displayMenu_cl();
+    printf("값을 입력하세요: ");
+    scanf("%d", &choice);
+
+    // 서버에 선택한 메뉴 전송
+    send(server_socket, &choice, sizeof(int), 0);
+
+    switch (choice) {
+        case 1:
+            system("clear");
+            startGame_cl(server_socket);
+            break;
+        case 2:
+            system("clear");
+            SearchMyRecord_cl(server_socket);
+            break;
+        case 3:
+            system("clear");
+            calculateAndFindMinAttempts_cl(server_socket);
+            break;
+        case 4:
+            close(server_socket);
+            exit(0);
+        default:
+            printf("올바른 선택이 아닙니다. 다시 선택하세요.\n");
+            break;
+    }
+}
+
+
+    return 0;
+}
+
+void displayMenu_cl() {
+    printf("\n");
+    printf("메뉴를 선택하세요:\n");
+    printf("1. 게임 시작\n");
+    printf("2. 나의 기록 검색\n");
+    printf("3. 최소값과 평균 \n");
+    printf("4. 게임 종료\n");
+}
+
+void startGame_cl(int server_socket) {
+    // 콘솔 창 새로
+    system("clear");
+
+    // 닉네임 입력
+    printf("사용할 아이디를 입력하세요 : ");
+    scanf("%s", players[numPlayers].name);
+    send(server_socket, players[numPlayers].name, sizeof(players[numPlayers].name), 0);
+    
+    //게임 시작 메세지
+    char startMessage[100];
+    recv(server_socket, &startMessage, sizeof(startMessage), 0);
+
+    printf("%s\n", startMessage);
+    
+    // 가득 찬 경우 시작 X
+    if (numPlayers == MAX_PLAYERS) {
+        printf("플레이어 기록이 가득 찼습니다. 더 이상 추가할 수 없습니다.\n");
+        return;
+    }
+
+    // 서버에서 랜덤 값을 받음
+    int randValues[3];
+    recv(server_socket, &randValues, sizeof(randValues), 0);
+
+    int attempts;
+    
+    printf("입력형식 000\n");
+    for (attempts = 1; attempts < 10; attempts++) {
+        printf("%d 번째 시도 - 값 입력 (또는 'exit' 입력하여 종료): ", attempts);
+        fflush(stdout);
+        int u1, u2, u3;
+        char userInput[5];
+
+        scanf("%s", userInput);
+
+        // 'exit'을 입력하면 게임 종료를 나타내는 -1을 서버에 전송
+        if (strcmp(userInput, "exit") == 0) {
+            printf("[Server] : 사용자가 게임을 종료했습니다.\n");
+            u1 = -1, u2 = -1, u3 = -1;
+            numPlayers++;
+            send(server_socket, &u1, sizeof(int), 0);
+            send(server_socket, &u2, sizeof(int), 0);
+            send(server_socket, &u3, sizeof(int), 0);
+            break;
+        } else {
+            // 정상적인 입력을 처리
+            sscanf(userInput, "%1d%1d%1d", &u1, &u2, &u3);
+        }
+        
+        // 입력 값 전송
+        send(server_socket, &u1, sizeof(int), 0);
+        send(server_socket, &u2, sizeof(int), 0);
+        send(server_socket, &u3, sizeof(int), 0);
+
+        // 서버에서 결과 받기
+        int strike, ball;
+        recv(server_socket, &strike, sizeof(int), 0);
+        recv(server_socket, &ball, sizeof(int), 0);
+
+        printf("[Server] : %dS %dB\n", strike, ball);
+
+        // 결과값 출력
+        if (strike == 3) {
+            printf("[Server] : 정답\n");
+            numPlayers++;
+            
+
+            break;
+        } else if (attempts == 9) {
+            printf("[Server] : 시도 횟수 초과 - 정답을 맞추지 못했습니다.\n");
+            printf("[Server] : 정답은 %d %d %d\n",randValues[0],randValues[1],randValues[2]);
+            numPlayers++;
+            break;
+        }
+
+    }
+
+}
+
+void SearchMyRecord_cl(int server_socket) {
+    // 사용자에게 찾을 플레이어 이름 입력 받음
+
+    system("clear");
+
+    char searchName[20];
+    printf("\n");
+    printf("찾고 싶은 플레이어의 이름을 입력하세요: ");
+    scanf("%s", searchName);
+
+    // 입력 받은 이름을 서버에 전송
+    send(server_socket, searchName, sizeof(searchName), 0);
+
+    // 서버에서 플레이어 정보 받음
+    struct Player searchResult;
+    recv(server_socket, &searchResult, sizeof(struct Player), 0);
+
+    printf("\n");
+    if (strcmp(searchResult.name, "Not Found") == 0) {
+        printf("찾는 플레이어가 없습니다.\n");
+    } else {
+        printf("플레이어: %s, 시도 횟수: %d\n", searchResult.name, searchResult.attempts);
+    }
+}
+// 시도 횟수 평균 계산 및 시도 횟수가 가장 적은 플레이어 정보 가져오기
+void calculateAndFindMinAttempts_cl(int server_socket) {
+    // 서버로부터 결과 문자열 수신
+    char resultMessage[200];
+    recv(server_socket, &resultMessage, sizeof(resultMessage), 0);
+
+    // 결과 출력
+    printf("[서버] :\n%s", resultMessage);
+}
---
+++ Game_Server.c
@@ -0,0 +1,274 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <time.h>
+
+#define PORT 12345
+#define MAX_PLAYERS 10
+
+struct Player {
+    char name[40];
+    int attempts;
+};
+
+struct Player players[MAX_PLAYERS];
+int numPlayers = 0;
+
+void startGame_sv(int client_socket);
+void generateUniqueRandomValues(int arr[], int size);
+void SearchMyRecord_sv(int client_socket);
+void closeConnection_sv(int server_socket, int client_socket);
+void calculateAndFindMinAttempts_sv(int client_socket);
+
+int main() {
+    int server_socket, client_socket;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_addr_size = sizeof(client_addr);
+
+    // 서버 소켓 생성
+    if ((server_socket = socket(PF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("Server socket creation error");
+        exit(1);
+    }
+
+    // 서버 주소 설정
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    // 서버 소켓 바인딩
+    if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+        perror("Server socket binding error");
+        close(server_socket);
+        exit(1);
+    }
+
+    // 클라이언트 대기 상태로 변경
+    if (listen(server_socket, 5) == -1) {
+        perror("Server listen error");
+        close(server_socket);
+        exit(1);
+    }
+
+    printf("서버 대기 중...\n");
+
+    // 클라이언트 접속 대기
+    if ((client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &client_addr_size)) == -1) {
+        perror("Server accept error");
+        close(server_socket);
+        exit(1);
+    }
+
+    printf("클라이언트 연결됨\n");
+
+    while (1) {
+    int choice;
+
+    recv(client_socket, &choice, sizeof(int), 0);
+
+    switch (choice) {
+        case 1:
+            startGame_sv(client_socket);
+            break;
+        case 2:
+            SearchMyRecord_sv(client_socket);
+            break;
+        case 3 :
+            calculateAndFindMinAttempts_sv(client_socket);
+            break;
+        case 4:
+            closeConnection_sv(server_socket, client_socket);
+            break;
+        default:
+            printf("올바른 선택이 아닙니다. 다시 선택하세요.\n");
+            break;
+    }
+}
+
+
+    return 0;
+}
+
+void startGame_sv(int client_socket) {
+        srand((unsigned int)time(NULL));
+
+    // 클라이언트 게임 시작 메세지 전달
+    recv(client_socket, players[numPlayers].name, sizeof(players[numPlayers].name), 0);
+    char startMessage[100];
+
+    snprintf(startMessage, sizeof(startMessage), "안녕하세요, %s님! 게임을 시작합니다.", players[numPlayers].name);
+    send(client_socket, &startMessage, sizeof(startMessage), 0);
+
+    printf("[Client] %s\n", startMessage);
+
+    // 클라이언트에게 랜덤 값을 전송
+    int randValues[3];
+    generateUniqueRandomValues(randValues, 3);
+    printf("[Server] 숫자야구 - 정답은 %d %d %d \n",randValues[0],randValues[1],randValues[2]);
+    // 서버에 값 전달
+    send(client_socket, randValues, sizeof(randValues), 0);
+
+    // 게임 로직 구현
+    int attempts;
+    
+    for (attempts = 1; attempts < 10; attempts++) {
+        int strike = 0;
+        int ball = 0;
+        // 사용자 입력 받기
+        int u1, u2, u3;
+        recv(client_socket,&u1 , sizeof(int), 0);
+        recv(client_socket,&u2 , sizeof(int), 0);
+        recv(client_socket,&u3 , sizeof(int), 0);
+
+        if (u1 == -1 && u2 == -1 && u3 == -1) {
+            printf("[Client] 게임 중단\n");
+            players[numPlayers].attempts = 9;
+            numPlayers++;
+            attempts = 9;
+            break;
+
+        }
+
+        // 서버에 기록 남기기
+    
+        printf("[Client] %d 번째 시도 - 값 %d %d %d \n", attempts,u1,u2,u3);
+        
+        // 과정 계산
+        strike += (randValues[0] == u1) + (randValues[1] == u2) + (randValues[2] == u3);
+        ball += (randValues[0] == u2) + (randValues[0] == u3) + (randValues[1] == u1) + (randValues[1] == u3) + (randValues[2] == u1) + (randValues[2] == u2);
+        
+        // 결과 전송
+        send(client_socket, &strike, sizeof(int), 0);
+        send(client_socket, &ball, sizeof(int), 0);
+        
+        // 서버에 결과과정 저장
+        printf("[Client] %d번째 시도 - %dS %dB\n", attempts,strike, ball);
+        
+        // 정답문구 제작
+        char correct_message[100] = "축하합니다! 정답을 맞추셨습니다.";
+
+        if (strike == 3) {
+            printf("[Client] 정답\n");
+            players[numPlayers].attempts = attempts;
+            numPlayers++;
+            break;
+        }else if (attempts == 9) {
+            // 플레이어가 9번 시도한 후에도 정답을 맞추지 못한 경우
+            printf("[Client] 시도 횟수 초과 - 정답을 맞추지 못했습니다.\n");
+            players[numPlayers].attempts = 9;  // 플레이어의 시도 횟수를 9로 설정
+            numPlayers++;
+            break;
+        }
+         
+    }
+        
+}   
+
+
+
+
+void SearchMyRecord_sv(int client_socket) {
+
+    // 클라이언트에게 찾을 플레이어 이름 요청
+    char searchName[20];
+    recv(client_socket, searchName, sizeof(searchName), 0);
+
+    int found = 0;
+    printf("\n");
+
+    for (int i = 0; i < numPlayers; i++) {
+        if (strcmp(players[i].name, searchName) == 0) {
+            // 찾은 플레이어 정보 전송
+            printf("[Server] 찾은 플레이어 - 이름: %s, 시도 횟수: %d\n", players[i].name, players[i].attempts);
+            send(client_socket, &players[i], sizeof(struct Player), 0);
+            found = 1;
+            break;
+        }
+    }
+
+    if (!found) {
+        // 플레이어를 찾지 못한 경우
+        printf("[Server] 찾는 플레이어가 없습니다.\n");
+        struct Player notFoundPlayer;
+        strcpy(notFoundPlayer.name, "Not Found");
+        notFoundPlayer.attempts = -1;  // 어떤 값을 넣을지는 상황에 따라 다름
+        send(client_socket, &notFoundPlayer, sizeof(struct Player), 0);
+    }
+}
+
+void generateUniqueRandomValues(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        int uniqueValue;
+        do {
+            uniqueValue = rand() % 10;
+            int isUnique = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[j] == uniqueValue) {
+                    isUnique = 0;
+                    break;
+                }
+            }
+            if (isUnique) {
+                break;
+            }
+        } while (1);
+
+        arr[i] = uniqueValue;
+    }
+}
+
+
+// 시도 횟수 평균 계산 및 시도 횟수가 가장 적은 플레이어 정보 반환하는 함수
+void calculateAndFindMinAttempts_sv(int client_socket) {
+    if (numPlayers == 0) {
+        char resultMessage[100];
+        snprintf(resultMessage, sizeof(resultMessage), "[서버] 아직 기록된 플레이어가 없습니다.");
+        send(client_socket, &resultMessage, sizeof(resultMessage), 0);
+        return;
+    }
+
+    int totalAttempts = 0;
+    int minAttempts = players[0].attempts;
+    int minIndex = 0;
+
+    for (int i = 0; i < numPlayers; i++) {
+        totalAttempts += players[i].attempts;
+
+        if (players[i].attempts < minAttempts) {
+            minAttempts = players[i].attempts;
+            minIndex = i;
+        }
+    }
+
+    float averageAttempts = (numPlayers > 0) ? (float)totalAttempts / numPlayers : 0.0;
+
+    // 결과 문자열 생성
+    char resultMessage[200];
+    if (numPlayers > 0) {
+        snprintf(resultMessage, sizeof(resultMessage), "[서버] 모든 플레이어의 시도 횟수 평균: %.2f\n[서버] 시도 횟수가 가장 적은 플레이어: %s (시도 횟수: %d)\n",
+                 averageAttempts, players[minIndex].name, minAttempts);
+    } else {
+        snprintf(resultMessage, sizeof(resultMessage), "[서버] 아직 기록된 플레이어가 없습니다.");
+    }
+
+    // 클라이언트에게 결과 전송
+    send(client_socket, resultMessage, sizeof(resultMessage), 0);
+}
+
+
+
+void closeConnection_sv(int server_socket, int client_socket) {
+    // 클라이언트와의 연결 종료
+    close(client_socket);
+
+    // 서버 소켓 종료
+    close(server_socket);
+
+    printf("프로그램을 종료합니다.\n");
+    exit(0);
+}
